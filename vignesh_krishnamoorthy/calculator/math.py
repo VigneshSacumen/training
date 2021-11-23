bbe6e37b45f486e215e7c88736463ff184b30ecc
vigneshsacumen <vignesh.k@sacumen.com>
2021-11-23 22:01:31 +0530

    Updated the script

--- vignesh_krishnamoorthy/calculator/math.py
+++ vignesh_krishnamoorthy/calculator/math.py
@@ -1,27 +1,29 @@
 
-from calculator import Calculator
+from Calculator import Calculator
 
-if _name_ == "__main__":
-    
-switcher = {
-    1: addition,
-    2: subtraction,
-    3: mul,
-    4: division,
-    5: module
-}
-def switch(operation, a, b):
-  return switcher.get(operation, default)(a, b)
 
-print('''Operation to perform
-1. Addition
-2. Subtraction
-3. Multiplication
-4. Division
-5. Module ''')
+if __name__ == "__main__":
 
-# Take input from user
-choice = int(input("Select operation from 1,2,3,4 : "))
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-print (switch(choice, a, b))
+    calc = Calculator()
+    switcher = {
+          1: 'addition',
+          2: 'subtraction',
+          3: 'mul',
+          4: 'div'
+        }
+
+   
+
+    print('''Operation to perform
+    1. Addition
+    2. Subtraction
+    3. Multiplication
+    4. Division ''')
+
+    # Take input from user
+    choice = int(input("Select operation from 1,2,3,4 : "))
+    operation = getattr(calc, choice)
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    result = operation(a, b)  
+    print ("Your result is...",result)
